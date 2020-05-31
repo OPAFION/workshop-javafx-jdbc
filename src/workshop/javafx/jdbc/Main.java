8c2679a9672f1b979e7fe8b8cba3de977842047d
Daniel Olivera <daniel13.oliveira.do@gmail.com>
2020-05-31 10:52:21 -0300

    Main View

--- src/workshop/javafx/jdbc/Main.java
+++ src/workshop/javafx/jdbc/Main.java
@@ -5,6 +5,7 @@
  */
 package workshop.javafx.jdbc;
 
+import java.io.IOException;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -15,16 +16,20 @@ import javafx.stage.Stage;
  *
  * @author DANIEL
  */
-public class WorkshopJavafxJdbc extends Application {
-    
+public class Main extends Application {
+
     @Override
-    public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
-        
-        Scene scene = new Scene(root);
-        
-        stage.setScene(scene);
-        stage.show();
+    public void start(Stage primaryStage) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/MainView.fxml"));
+            Parent parent = loader.load();
+            Scene mainScene = new Scene(parent);
+            primaryStage.setScene(mainScene);
+            primaryStage.setTitle("Sample JavaFX application");
+            primaryStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     /**
@@ -33,5 +38,5 @@ public class WorkshopJavafxJdbc extends Application {
     public static void main(String[] args) {
         launch(args);
     }
-    
+
 }
